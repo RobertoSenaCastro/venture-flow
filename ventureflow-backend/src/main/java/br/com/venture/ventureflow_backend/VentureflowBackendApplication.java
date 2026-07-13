de64952ee076a0785fc127af82c4b903c407a1bd
Roberto Sena <robertosena721@gmail.com>
2026-07-13 08:49:54 -0300

    first commit

---
+++ ventureflow-backend/src/main/java/br/com/venture/ventureflow_backend/VentureflowBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.venture.ventureflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentureflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentureflowBackendApplication.class, args);
+	}
+
+}
---
+++ ventureflow-backend/src/test/java/br/com/venture/ventureflow_backend/VentureflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.venture.ventureflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentureflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
